34ac519188daf0bd36c02a41bb55c1912ee620a3
anonymous99-Rise <132993532+anonymous99-Rise@users.noreply.github.com>
2026-07-27 11:14:53 +0000

    Deploying to gh-pages from @ anonymous99-Rise/CVE@dd6dbb4cb2cc7fd54ddb81bb39a9b0e803862146 🚀

---
+++ _next/static/JrQx8DzXOFsKiqaS3BcNF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/474-2cefec5eb8addec0.js","static/chunks/pages/index-b3fa6fabdd48a1ae.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
